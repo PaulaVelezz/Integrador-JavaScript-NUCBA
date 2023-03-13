0c9f2e70a58ad07876572ab5c5921a845c562d26
PaulaVelezz <velezpaula.a@gmail.com>
2023-03-13 00:57:39 -0300

    trabajando en el carrito

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const productsData = [
 		price: 600,
 		category: "Destacados",
         description: "Tomates Cherrys y Alcachofas.",
-		img: "assets/focaccia 1-14564802.jpg",
+		img: "./assets/focaccia 1-14564802.jpg",
 	},
 	{
 		id: 2,
@@ -14,7 +14,7 @@ const productsData = [
 		price: 650,
 		category: "Vegano",
         description: "Cebolla Caramelizada y Tomillo.",
-		img: "assets/focaccia 2-12335533.jpg",
+		img: "./assets/focaccia 2-12335533.jpg",
 	},
     {
 		id: 3,
@@ -22,7 +22,7 @@ const productsData = [
 		price: 750,
 		category: "Nuevos",
         description:"Cherrys, Alcachofas y Cebolla Morada.",
-		img: "assets/focaccia 7.jpg",
+		img: "./assets/focaccia 7.jpg",
 	},
     {
 		id: 4,
@@ -30,7 +30,7 @@ const productsData = [
 		price: 500,
 		category: "Vegetariano",
         description:"Cherrys, Aceitunas Verdes y Romero.",
-		img:"assets/focaccia 8.jpg",
+		img:"./assets/focaccia 8.jpg",
 	},
     {
 		id: 5,
@@ -38,7 +38,7 @@ const productsData = [
 		price: 550,
 		category: "Vegano",
         description:"Aceitunas Negras, Tomillo y Romero.",
-		img: "assets/focaccia 10.jpg",
+		img: "./assets/focaccia 10.jpg",
 	},
     {
 		id: 6,
@@ -46,7 +46,7 @@ const productsData = [
 		price: 700,
 		category: "Vegetariano",
         description: "Cebolla Caramelizada, Mozzarella y Albaca.",
-		img: "assets/focaccia 15.jpg",
+		img: "./assets/focaccia 15.jpg",
 	},
     {
 		id: 7,
@@ -54,7 +54,7 @@ const productsData = [
 		price: 650,
 		category: "Destacados",
         description: "Mix de Cherrys y Cebolla Morada.",
-		img: "assets/focaccia 6.webp",
+		img: "./assets/focaccia 6.webp",
 	},
     {
 		id: 8,
@@ -62,7 +62,7 @@ const productsData = [
 		price: 750,
 		category: "Nuevos",
         description: "Camarones, Jamón, Zuccini y Pesto.",
-		img: "assets/focaccia 17.jpg",
+		img: "./assets/focaccia 17.jpg",
 	},
     {
 		id: 9,
@@ -70,7 +70,7 @@ const productsData = [
 		price: 750,
 		category: "Destacados",
         description: "Higos, Queso Azul, Miel y Albaca.",
-		img: "assets/focaccia 18.jpg",
+		img: "./assets/focaccia 18.jpg",
 	},
     {
 		id: 10,
@@ -78,7 +78,7 @@ const productsData = [
 		price: 500,
 		category: "Vegano",
         description: "Aceite de oliva, Sal Gruesa y Romero.",
-		img: "assets/focaccia 6.jpg",
+		img: "./assets/focaccia 6.jpg",
 	},
     {
 		id: 11,
@@ -86,7 +86,7 @@ const productsData = [
 		price: 150,
 		category: "Dips",
         description: "Aceite de Oliva y Aceto Balsamico.",
-		img: "assets/aceites.jpg",
+		img: "./assets/aceites.jpg",
 	},
     {
 		id: 12,
@@ -94,7 +94,7 @@ const productsData = [
 		price: 100,
 		category: "Dips",
         description: "Aceite de Oliva con Romero y Tomillo.",
-		img: "assets/aceites 2.jpg",
+		img: "./assets/aceites 2.jpg",
 	},
 ]
 // division de cards
@@ -122,11 +122,32 @@ const categoriesList = document.querySelectorAll(".category");
 // Botón de ver más
 const btn__load = document.querySelector(".btn__load");
 
+const cart__btn = document.querySelector(".cart__btn");
+//carrito menu
+const cart__menu = document.querySelector(".cart");
 // abrir y cerrar menu
 const btn__bar = document.querySelector(".menu__label");
 //menu
 const bars__menu = document.querySelector (".navbar__list");
 
+const productsCarrito = document.querySelector(".cart__container");
+
+const total = document.querySelector (".total");
+
+const deleteBtn = document.querySelector(".btn__delete");
+const buyBtn = document.querySelector(".btn__buy");
+
+//probando otra forma de armar el carrito
+const openPanelBtn = document.getElementById("open-panel");
+const closePanelBtn = document.getElementById("close-panel");
+const cartPanel = document.querySelector (".cart");
+
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const saveLocalStorage = (cartList) => {
+	localStorage.setItem("cart", JSON.stringify(cartList));
+};
 
 const renderProduct = (product) => {
 	const {id, name, price, description, img } = product;
@@ -142,7 +163,7 @@ const renderProduct = (product) => {
 				class="btn-add",
 				data-id="${id}"
 				data-name="${name}"
-			    data-price="${price}
+			    data-price="${price}"
 			    data-img="${img}"
 				data-description="${description}"
 		             > Agregar <i class="fa-solid fa-cart-plus"></i>
@@ -227,13 +248,247 @@ const showMoreProducts = () => {
 
 const toggleMenu = () => {
 	bars__menu.classList.toggle("open-menu");
+	if (cart__menu.classList.contains
+		("open-cart")) {
+			cart__menu.classList.remove
+			("open-cart")
+			return;
+		}
+		overlay.classList.toggle
+		("show-overley");
+};
+
+const toggleCart = () =>{
+	cart__menu.classList.toggle("open-cart");
+	if (bars__menu.classList.contains("open-menu")) {
+		bars__menu.classList.remove
+		("open-menu");
+		return;
+	}
+	overlay.classList.toggle
+	("show-overley");
+};
+
+const closeOnClick = (e) => {
+	if (!e.target.classList.contains("navbar__links")) {
+		return;
+	}
+	bars__menu.classList.remove("open-menu");
+	overlay.classList.remove("show-overlay");
+};
+
+const closeOnScroll = () => {
+	if (
+		!bars__menu.classList.contains
+		("open-menu") && 
+		!cart__menu.classList.contains
+		("open-cart")
+		) {
+		return;
+	}
+	bars__menu.classList.remove("open-menu");
+	cart__menu.classList.remove("open-cart");
+	overlay.classList.remove
+	("show-overley");
+};
+
+//probando otra forma de armar el carrito
+openPanelBtn.onclick = () => {
+	cartPanel.classList.add("open");
+	openPanelBtn.classList.add("hide");
+	bars__menu.classList.remove("open-menu");
+};
+
+closePanelBtn.onclick = () => {
+	cartPanel.classList.remove("open");
+	openPanelBtn.classList.remove("hide");
+};
+
+const renderCarritoProduct = (carritoProduct) => {
+	const {id, name, img, quantity, price} = carritoProduct;
+	return `
+	<div class="cart__item">
+		<img src="${img}" alt="">
+		<div class="item__data">
+			<h3 class="item__title">${name}</h3>
+			<span class="item__price">$ ${price}</span>
+		</div>
+		<div class="item__quantity__controller">
+			<span class="quantity__controller__down" data-id=${id}> - </span>
+			<span class="item__quantity">${quantity}</span>
+			<span class="quantity__controller__up" data-id=${id}> + </span>
+		</div>
+	</div>
+	`;
+};
+
+const renderCarrito = () => {
+	if (!cart.length) {
+		productsCarrito.innerHTML = `<p class= "empty__msg"> No hay productos en tu carrito.</p>`;
+		return;
+	}
+	productsCarrito.innerHTML = cart.map(renderCarritoProduct).join("");
+};
+
+const getCarritoTotal = () => {
+	return cart.reduce((acc, cur) => {
+		return acc + Number(cur.price) * cur.quantity;
+	}, 0);
+};
+
+const showTotal = () => {
+	total.innerHTML = `${getCarritoTotal()} $`;
+};
+
+const disableBtn = (btn) => {
+	if(!cart.length) {
+		btn.classList.add("disabled");
+	} else {
+		btn.classList.remove("disabled");
+	}
+};
+
+const checkCartState = () => {
+	saveLocalStorage(cart);
+	renderCarrito();
+	showTotal();
+	disableBtn(buyBtn);
+	disableBtn(deleteBtn);
+};
+
+const addCartProduct = (e) => {
+	if (!e.target.classList.contains("btn-add")) {
+		return;
+	}
+	const {id, name, price, img} = e.target.dataset;
+
+	const product = productData (id, name, price, img);
+
+	if (cartProductExists(product)) {
+		addUnittoProduct(product);
+	} else {
+		createProductCart(product);
+	}
+
+	checkCartState();
+};
+
+const productData = (id, name, price, img) => {
+	return {id, name, price, img};
+};
+
+const cartProductExists = (product) => {
+	return cart.find((item) => {
+		return item.id === product.id;
+	});
+};
+
+const addUnittoProduct = (product) =>{
+	cart = cart.map((carritoProduct) => {
+		return carritoProduct.id === product.id 
+		? {...carritoProduct, quantity: carritoProduct.
+			quantity +1} 
+			: carritoProduct;
+	});
+};
+
+const createProductCart = (product) =>{
+	cart = [
+		...cart,
+		{
+			...product,
+			quantity: 1,
+		},
+	];
+};
+
+
+const quantityControllerDownBtn = (id) => {
+	const existingCartProduct = cart.find((item) => {
+		return item.id === id;
+	});
+
+	if (existingCartProduct.quantity === 1) {
+		if (window.confirm ("¿Desea eliminar el producto del carrito?")) 
+		{
+			removeProductfromCart(existingCartProduct);
+		}
+		return;
+	}
+	removeProductUnit(existingCartProduct);
+};
+
+const removeProductfromCart = (existingProduct) => {
+	cart = cart.filter ((product) => product.id !== 
+	existingProduct.id);
+	checkCartState();
+};
+
+const removeProductUnit = (existingProduct) => {
+	cart = cart.map((product) => {
+		return product.id === existingProduct.id ?
+		 {...product, quantity: Number(product.quantity) - 1}
+		  : product;
+	});
+};
+
+const quantityControllerUpBtn = (id) => {
+	const existingCartProduct = cart.find((item) => {
+		return item.id === id;
+	});
+
+	addUnittoProduct(existingCartProduct);
+};
+
+const quantityController = (e) => {
+	if (e.target.classList.contains("quantity__controller__down")) {
+		quantityControllerDownBtn(e.target.dataset.id)
+	} else if (e.target.classList.contains("quantity__controller__up")) {
+		quantityControllerUpBtn(e.target.dataset.id)
+	}
+	checkCartState();
+};
+
+const vaciamosCartItems = () => {
+	cart = [];
+	checkCartState();
+};
+
+const validacionCartAction = (confirmMsg, successMsg) =>{
+	if(!cart.length) return;
+
+	if (window.confirm(confirmMsg)) {
+		vaciamosCartItems();
+		alert(successMsg);
+	}
+};
+
+const buyComplete = () => {
+	validacionCartAction("¿Desea finalizar su compra?",
+	"¡Gracias por su compra!");
+};
+
+const deleteCart = () =>{
+	validacionCartAction("¿Desea vaciar su carrito?",
+	"Su carrito se vacio con exito!");
 };
 
 const init = () => {
-    renderProducts ();
+    renderProducts();
     categories.addEventListener("click",applyFilter);
     btn__load.addEventListener ("click",showMoreProducts);
 	btn__bar.addEventListener("click",toggleMenu);
+	cart__btn.addEventListener("click",toggleCart);
+	bars__menu.addEventListener("click", closeOnClick);
+	window.addEventListener("scroll", closeOnScroll);
+	document.addEventListener("DOMContentLoaded", renderCarrito);
+	document.addEventListener("DOMContentLoaded", showTotal);
+	products.addEventListener("click", addCartProduct);
+	productsCarrito.addEventListener("click", quantityController);
+	buyBtn.addEventListener("click", buyComplete);
+	deleteBtn.addEventListener("click", deleteCart);
+	disableBtn(buyBtn);
+	disableBtn(deleteBtn);
 };
 
 init();
